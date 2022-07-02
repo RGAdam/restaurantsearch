1e065b0dd4d7002d17bc8b88cd87a110a4318159
Rehák Ádám <massiveadamgaming@gmail.com>
2022-07-02 13:05:38 +0200

    feat: category list

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, View } from "react-native";
+import CategoryItem from "./src/components/CategoryItem";
 import Header from "./src/components/Header";
 import Search from "./src/components/Search";
 
 export default function App() {
+  const [term, setTerm] = useState("Burger");
+  const commonCategories = [
+    {
+      name: "Burger",
+      imageUrl: require("./src/assets/images/burger.png"),
+    },
+    {
+      name: "Pizza",
+      imageUrl: require("./src/assets/images/pizza.png"),
+    },
+    {
+      name: "Dessert",
+      imageUrl: require("./src/assets/images/cake.png"),
+    },
+    {
+      name: "Drinks",
+      imageUrl: require("./src/assets/images/smoothies.png"),
+    },
+    {
+      name: "Steak",
+      imageUrl: require("./src/assets/images/steak.png"),
+    },
+    {
+      name: "Pasta",
+      imageUrl: require("./src/assets/images/pasta.png"),
+    },
+  ];
+
   return (
-    <View style={styles.container}>
+    <View>
       <Header />
       <Search />
+      <FlatList
+        data={commonCategories}
+        renderItem={({ item, index }) => {
+          return (
+            <CategoryItem
+              name={item.name}
+              imageUrl={item.imageUrl}
+              index={index}
+              active={item.name === term}
+              handlePress={() => setTerm(item.name)}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => category.name}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
-});
